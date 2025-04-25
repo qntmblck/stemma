475d6f8a5142d21102e0fa22cb6e33b1bce17f78
qntmblck <jcomeaux@ug.uchile.cl>
2025-04-25 11:46:48 -0400

    master

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -95,7 +95,7 @@ const logout = () => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                       </svg>
                     </button>
-                  </span>
+                  </span>ñ
                 </template>
                 <template #content>
   <div class="block px-4 py-2 text-xs text-gray-400">Gestionar cuenta</div>
