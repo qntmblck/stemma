2105b9da147e2ca0479b94bfad167aa9e3441159
qntmblck <jcomeaux@ug.uchile.cl>
2025-04-25 14:38:31 -0400

    master

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -25,10 +25,10 @@
         <!-- Navegación Escritorio -->
         <div class="hidden lg:flex items-center space-x-8 ml-[-2rem]">
           <a :href="route('home')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Inicio</a>
-          <a :href="route('nosotros')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Nosotros</a>
-          <a :href="route('proyectos')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Proyectos</a>
-          <a :href="route('catalogo')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Catálogo</a>
-          <a :href="route('contacto')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Contacto</a>
+          <a :href="route('home')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Nosotros</a>
+          <a :href="route('home')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Proyectos</a>
+          <a :href="route('home')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Catálogo</a>
+          <a :href="route('home')" class="text-sm font-bold text-gray-800 hover:text-yellow-600 transition">Contacto</a>
         </div>
 
         <!-- Botones Escritorio -->
@@ -59,10 +59,10 @@
         class="lg:hidden bg-gradient-to-tr from-white via-yellow-50 to-yellow-100 text-gray-800 px-4 pt-4 pb-6 space-y-4 shadow-md"
       >
         <a :href="route('home')" class="block text-sm font-bold hover:text-yellow-600 transition">Inicio</a>
-        <a :href="route('nosotros')" class="block text-sm font-bold hover:text-yellow-600 transition">Nosotros</a>
-        <a :href="route('proyectos')" class="block text-sm font-bold hover:text-yellow-600 transition">Proyectos</a>
-        <a :href="route('catalogo')" class="block text-sm font-bold hover:text-yellow-600 transition">Catálogo</a>
-        <a :href="route('contacto')" class="block text-sm font-bold hover:text-yellow-600 transition">Contacto</a>
+        <a :href="route('home')" class="block text-sm font-bold hover:text-yellow-600 transition">Nosotros</a>
+        <a :href="route('home')" class="block text-sm font-bold hover:text-yellow-600 transition">Proyectos</a>
+        <a :href="route('home')" class="block text-sm font-bold hover:text-yellow-600 transition">Catálogo</a>
+        <a :href="route('home')" class="block text-sm font-bold hover:text-yellow-600 transition">Contacto</a>
         <hr class="my-2 border-yellow-200" />
         <a :href="route('login')" class="block text-sm font-bold text-yellow-700 hover:text-yellow-800 transition">Ingresar</a>
       </div>
